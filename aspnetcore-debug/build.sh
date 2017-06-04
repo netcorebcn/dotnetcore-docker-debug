2ba37d2bb750179716d13d19334d5e89fb4cf4e2
paulopez78 <pau.lopez.78@gmail.com>
2017-06-04 22:48:35 +0200

    add version 2 of netcore

--- aspnetcore-debug/build.sh
+++ aspnetcore-debug/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker build -t paulopez/aspnetcore-debug .
-docker push paulopez/aspnetcore-debug
+docker build -t paulopez/aspnetcore-debug:latest -f ./1.1/Dockerfile
+docker push paulopez/aspnetcore-debug:latest
+
+docker build -t paulopez/aspnetcore-debug:2 -f ./2.0/Dockerfile .
+docker push paulopez/aspnetcore-debug:2
